8be3a11f98df97e351407cd113d80e9d54d2aa73
Lucas <lucas.marsalha@gmail.com>
2024-06-20 21:48:37 -0300

    lucas

--- analisador_de_texto/funcoes.c
+++ analisador_de_texto/funcoes.c
@@ -1,51 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
 
-void buscar_palavra(char texto[1001]){
-	FILE *arquivo;
-	
-	arquivo = fopen("texto.txt", "r");
-	
-	char palavra[100], linha[100];
-	int count,i, encontrado = 0;
-	
-	printf("Informe o nome da palvara: ");
-	scanf("%s", palavra);
-	
+int palavra_impressa(char palavra_texto[], FILE *palavras_unicas) {
+    // retorna para o início do arquivo
+	rewind(palavras_unicas);
+    
+    char palavra[100];
+    
+	// EOF -> End Of File
+    while (fscanf(palavras_unicas, "%s", palavra) != EOF) {
+        if (strcmp(palavra, palavra_texto) == 0) {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+void listar_palavras_diferentes() {
+    char palavra_texto[100];
+    int i;
+    
+    FILE *texto, *palavras_unicas;
+    
+    texto = fopen("texto.txt", "r");
+    if (texto == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+    
+    palavras_unicas = fopen("palavras_unicas.txt", "w");
+    
+    if (palavras_unicas == NULL) {
+        printf("Erro ao criar o arquivo de palavras unicas.\n");
+        exit(1);
+    }
+    
+	// EOF -> End Of File
+    // percorre o arquivo lendo palavra por palavra
+    while (fscanf(texto, "%s", palavra_texto) != EOF) {
+        // verifica se a palavra já foi impressa
+        if (!palavra_impressa(palavra_texto, palavras_unicas)) {
+            // imprime a palavra no arquivo de palavras unicas
+			fprintf(palavras_unicas, "%s\n", palavra_texto);
+        }
+    }
+    
+    fclose(texto);
+    fclose(palavras_unicas);
+    
+    // Exibe as palavras únicas
+    palavras_unicas = fopen("palavras_unicas.txt", "r");
+    printf("Palavras diferentes no texto:\n");
+    
+    while (fscanf(palavras_unicas, "%s", palavra_texto) != EOF) {
+        printf("%s\n", palavra_texto);
+    }
+    
+    fclose(palavras_unicas);
+    
+	system("pause");
 	system("cls");
-	count = 0;
-	//lendo as linhas do arquivo txt
-	while(fgets(linha, sizeof(linha), arquivo) != NULL){
-		// buscando a ocorrencia da palavra informada pelo usuario
-		if(strstr(linha ,palavra) != NULL){
-			count++;
-			encontrado = 1;
-		}
+    main();
+}
+
+void buscar_palavra() {
+    char palavra[100], palavra_busca[100];
+    bool encontrado = false;
+    int ocorrencias = 0;
+    int i;
+    
+	printf("Informe a palvara: ");
+	scanf("%s", palavra);
+    
+    FILE *arquivo;
+    arquivo = fopen("texto.txt", "r");
+    
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo de texto.\n");
+        exit(1);
+    }
+    
+    // percorre o arquivo lendo palavra por palavra
+    while (fscanf(arquivo, "%s", palavra_busca) != EOF) {
+        // verifica se a palavra encontrada eh igual a palavra de busca
+        if (strcmp(palavra, palavra_busca) == 0) {
+            ocorrencias++;
+            encontrado = true;
+        }
+    }
+    
+    fclose(arquivo);
+    
+    if (encontrado) {
+    	printf("A palavra '%s' aparece %d vezes no texto.\n", palavra, ocorrencias);
+	} else {
+		printf("A palavra '%s' nao aparece no texto.\n");	
 	}
-	
-	
-	if(encontrado){
-		printf("Quantidade de ocorrencias da palavra '%s' foi de %d vezes\n\n", palavra, count-1);
-		
-	}else{
-		puts("palavra não encontrada\n\n");
-	}
-	
-	
 	
 	system("pause");
 	system("cls");
+	main();
 }
 
 void menu(){
 	FILE *arquivo;
 	char c, opcao, palavra_texto[100], texto[1001];
-	int i;
 	
 	arquivo = fopen("texto.txt", "r");
-	
 	
 	if(arquivo == NULL){
 		system("cls");
@@ -53,21 +118,18 @@ void menu(){
 		exit(1);
 	}
 	
-	i=0;
-	
 	// listando palavras
 	// EOF -> End Of File
 	while((fscanf(arquivo, "%s", palavra_texto)) != EOF){ 
 		printf("%s ", palavra_texto);
 	}
 	
-
-	
 	fclose(arquivo);	
 	
-	puts("\n\nDeseja ver a quantidade de ocorrencias de uma palavra?\n");
-	puts("[ 1 ] - Sim");
-	puts("[ 0 ] - Nao");
+	puts("\n");
+	puts("[ 1 ] - Listar palavras diferentes");
+	puts("[ 2 ] - Buscar quantidade de ocorrencias de uma palavra");
+	puts("[ 0 ] - Sair");
 	printf("\nInforme a opcao escolhida: ");
 	
 	opcao = getchar();
@@ -77,7 +139,11 @@ void menu(){
 	switch(opcao){
 		case '1':
 			system("cls");
-			buscar_palavra(texto);
+			listar_palavras_diferentes();
+			break;
+		case '2':
+			system("cls");
+			buscar_palavra();
 			break;
 		case '0':
 			system("cls");
@@ -85,7 +151,5 @@ void menu(){
 			exit(1);
 			break;
 	}
-	
 }
-
 
--- analisador_de_texto/main.c
+++ analisador_de_texto/main.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "funcoes.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
 void main() {
-	
 	puts("Analisador de texto\n");
-	
 	system("pause");
 	system("cls");
-	menu();
+	while (true) {
+		menu();	
+	}
 }
-
-
-
 
